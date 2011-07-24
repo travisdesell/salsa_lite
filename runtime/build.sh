f431a5e3a2b0003be46a0a114fd66243e4a535f1
Travis Desell <travis.desell@gmail.com>
2011-07-24 17:28:10 -0400

    updated build script for runtime

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,8 +1,8 @@
 cd language
-java -Dlocal_fcs salsa_lite.compiler.SalsaCompiler *.salsa
+java salsa_lite.compiler.SalsaCompiler *.salsa
 javac *.java ./exceptions/*.java
 cd ../io
-java -Dlocal_fcs salsa_lite.compiler.SalsaCompiler *.salsa
+java salsa_lite.compiler.SalsaCompiler *.salsa
 javac *.java
 cd ..
 javac *.java
