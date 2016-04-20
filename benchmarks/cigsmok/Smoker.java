e4540576f06b76c1ef5dddde3c95792267596e35
Brent Lee <brentlee@dhcp-134-129-216-64.cs.und.edu>
2016-04-20 15:01:30 -0500

    debug messages removed

---
+++ benchmarks/cigsmok/Smoker.java
@@ -0,0 +1,164 @@
+package salsa_lite.benchmarks.cigsmok;
+
+/****** SALSA LANGUAGE IMPORTS ******/
+import salsa_lite.common.DeepCopy;
+import salsa_lite.runtime.LocalActorRegistry;
+import salsa_lite.runtime.Hashing;
+import salsa_lite.runtime.Acknowledgement;
+import salsa_lite.runtime.SynchronousMailboxStage;
+import salsa_lite.runtime.Actor;
+import salsa_lite.runtime.Message;
+import salsa_lite.runtime.RemoteActor;
+import salsa_lite.runtime.MobileActor;
+import salsa_lite.runtime.StageService;
+import salsa_lite.runtime.TransportService;
+import salsa_lite.runtime.language.Director;
+import salsa_lite.runtime.language.JoinDirector;
+import salsa_lite.runtime.language.MessageDirector;
+import salsa_lite.runtime.language.ContinuationDirector;
+import salsa_lite.runtime.language.TokenDirector;
+
+import salsa_lite.runtime.language.exceptions.RemoteMessageException;
+import salsa_lite.runtime.language.exceptions.TokenPassException;
+import salsa_lite.runtime.language.exceptions.MessageHandlerNotFoundException;
+import salsa_lite.runtime.language.exceptions.ConstructorNotFoundException;
+
+/****** END SALSA LANGUAGE IMPORTS ******/
+
+
+public class Smoker extends salsa_lite.runtime.Actor implements java.io.Serializable {
+
+
+    public Object writeReplace() throws java.io.ObjectStreamException {
+        int hashCode = Hashing.getHashCodeFor(this.hashCode(), TransportService.getHost(), TransportService.getPort());
+        synchronized (LocalActorRegistry.getLock(hashCode)) {
+            LocalActorRegistry.addEntry(hashCode, this);
+        }
+        return new SerializedSmoker( this.hashCode(), TransportService.getHost(), TransportService.getPort() );
+    }
+
+    public static class RemoteReference extends Smoker {
+        private int hashCode;
+        private String host;
+        private int port;
+        RemoteReference(int hashCode, String host, int port) { this.hashCode = hashCode; this.host = host; this.port = port; }
+
+        public Object invokeMessage(int messageId, Object[] arguments) throws RemoteMessageException, TokenPassException, MessageHandlerNotFoundException {
+            TransportService.sendMessageToRemote(host, port, this.getStage().message);
+            throw new RemoteMessageException();
+        }
+
+        public void invokeConstructor(int messageId, Object[] arguments) throws RemoteMessageException, ConstructorNotFoundException {
+            TransportService.sendMessageToRemote(host, port, this.getStage().message);
+            throw new RemoteMessageException();
+        }
+
+        public Object writeReplace() throws java.io.ObjectStreamException {
+            return new SerializedSmoker( hashCode, host, port);
+        }
+    }
+
+    public static class SerializedSmoker implements java.io.Serializable {
+        int hashCode;
+        String host;
+        int port;
+
+        SerializedSmoker(int hashCode, String host, int port) { this.hashCode = hashCode; this.host = host; this.port = port; }
+
+        public Object readResolve() throws java.io.ObjectStreamException {
+            int hashCode = Hashing.getHashCodeFor(this.hashCode, this.host, this.port);
+            synchronized (LocalActorRegistry.getLock(hashCode)) {
+                Smoker actor = (Smoker)LocalActorRegistry.getEntry(hashCode);
+                if (actor == null) {
+                    RemoteReference remoteReference = new RemoteReference(this.hashCode, this.host, this.port);
+                    LocalActorRegistry.addEntry(hashCode, remoteReference);
+                    return remoteReference;
+                } else {
+                    return actor;
+                }
+            }
+        }
+    }
+
+    public String getMessageInformation(int messageId) {
+    	switch (messageId) {
+    		case 0: return "java.lang.String [salsa_lite.runtime.Actor].toString()";
+    		case 1: return "int [salsa_lite.runtime.Actor].hashCode()";
+    		case 2: return "int [salsa_lite.runtime.Actor].getStageId()";
+    		case 3: return "ack [salsa_lite.benchmarks.cigsmok.Smoker].StartSmoke(long)";
+    	}
+    	return "No message with specified id.";
+    }
+
+    public Smoker() { super(); }
+    public Smoker(int stage_id) { super(stage_id); }
+
+    Arbiter arbiter;
+    int id;
+
+
+    public Object invokeMessage(int messageId, Object[] arguments) throws RemoteMessageException, TokenPassException, MessageHandlerNotFoundException {
+        switch(messageId) {
+            case 0: return toString();
+            case 1: return hashCode();
+            case 2: return getStageId();
+            case 3: StartSmoke( (Long)arguments[0] ); return null;
+            default: throw new MessageHandlerNotFoundException(messageId, arguments);
+        }
+    }
+
+    public void invokeConstructor(int messageId, Object[] arguments) throws RemoteMessageException, ConstructorNotFoundException {
+        switch(messageId) {
+            case 0: construct( (Arbiter)arguments[0], (Integer)arguments[1] ); return;
+            default: throw new ConstructorNotFoundException(messageId, arguments);
+        }
+    }
+
+    public void construct(Arbiter a, int i) {
+        id = i;
+        arbiter = a;
+    }
+
+
+
+    public void StartSmoke(long time) {
+        StageService.sendMessage(arbiter, 3 /*StartedSmoking*/, null);
+        try {
+            Thread.sleep(time);
+        }
+        catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    public static Smoker construct(int constructor_id, Object[] arguments) {
+        Smoker actor = new Smoker();
+        StageService.sendMessage(new Message(Message.CONSTRUCT_MESSAGE, actor, constructor_id, arguments));
+        return actor;
+    }
+
+    public static Smoker construct(int constructor_id, Object[] arguments, int target_stage_id) {
+        Smoker actor = new Smoker(target_stage_id);
+        actor.getStage().putMessageInMailbox(new Message(Message.CONSTRUCT_MESSAGE, actor, constructor_id, arguments));
+        return actor;
+    }
+
+    public static TokenDirector construct(int constructor_id, Object[] arguments, int[] token_positions) {
+        Smoker actor = new Smoker();
+        TokenDirector output_continuation = TokenDirector.construct(0 /*construct()*/, null);
+        Message input_message = new Message(Message.CONSTRUCT_CONTINUATION_MESSAGE, actor, constructor_id, arguments, output_continuation);
+        MessageDirector md = MessageDirector.construct(3, new Object[]{input_message, arguments, token_positions});
+        return output_continuation;
+    }
+
+    public static TokenDirector construct(int constructor_id, Object[] arguments, int[] token_positions, int target_stage_id) {
+        Smoker actor = new Smoker(target_stage_id);
+        TokenDirector output_continuation = TokenDirector.construct(0 /*construct()*/, null, target_stage_id);
+        Message input_message = new Message(Message.CONSTRUCT_CONTINUATION_MESSAGE, actor, constructor_id, arguments, output_continuation);
+        MessageDirector md = MessageDirector.construct(3, new Object[]{input_message, arguments, token_positions}, target_stage_id);
+        return output_continuation;
+    }
+
+}
